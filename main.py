a9d2bd0cf46d3569a8822920ffcb59cae7773688
Arturo Filastò <arturo@filasto.net>
2023-08-29 18:38:08 +0200

    Implement workaround to https://github.com/meejah/txtorcon/issues/389

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import os
+from pathlib import Path
 import csv
 from datetime import datetime
 import random
@@ -6,11 +8,12 @@ import random
 from tqdm import tqdm
 
 from twisted.internet.task import react
-from twisted.internet.defer import inlineCallbacks, ensureDeferred
+from twisted.internet.defer import inlineCallbacks, ensureDeferred, TimeoutError, DeferredList
 from twisted.internet.endpoints import UNIXClientEndpoint
 
 import treq
 import txtorcon
+from txtorcon.torcontrolprotocol import TorDisconnectError
 
 URL_LIST = [
     "https://ooni.org/robots.txt",
@@ -43,13 +46,14 @@ async def get_exit_list(reactor):
     return exit_list
 
 
-async def measure_relay(reactor, tor, tor_state, relay, url_list, csvwriter):
-    exit_fp = relay["fingerprint"]
-    ns_info = await tor.protocol.get_info("ns/id/" + exit_fp)
-    md_info = await tor.protocol.get_info("md/id/" + exit_fp)
+async def measure_relay(reactor, tor_launcher, relay, url_list, csvwriter):
+    tor = tor_launcher.tor
+    tor_state = tor_launcher.state
+    await tor.protocol.get_info("ns/id/" + relay["fingerprint"])
+    await tor.protocol.get_info("md/id/" + relay["fingerprint"])
 
     first_hop = random.choice(list(tor_state.guards.values()))
-    exit_hop = tor_state.routers_by_hash["$" + exit_fp]
+    exit_hop = tor_state.routers_by_hash["$" + relay["fingerprint"]]
     tqdm.write(f"Creating a circuit via {relay['nickname']} ({relay['fingerprint']})")
     circ = await tor_state.build_circuit([first_hop, exit_hop], using_guards=False)
     await circ.when_built()
@@ -58,9 +62,9 @@ async def measure_relay(reactor, tor, tor_state, relay, url_list, csvwriter):
 
     config = await tor.get_config()
     for url in url_list:
-        tqdm.write(f"* fetching {url} over {exit_fp}")
+        tqdm.write(f"* fetching {url} over {relay['fingerprint']}")
         row = {
-            "exit_fp": exit_fp,
+            "exit_fp": relay["fingerprint"],
             "exit_nickname": relay["nickname"],
             "exit_cc": relay["country"],
             "exit_asn": relay["as"],
@@ -84,13 +88,53 @@ async def measure_relay(reactor, tor, tor_state, relay, url_list, csvwriter):
         csvwriter.writerow(row)
 
 
-class TorLogger:
-    def __init__(self, prefix=""):
-        self.prefix = prefix
+# This class is currently needed as a workaround to: https://github.com/meejah/txtorcon/issues/389
+# Depending on how and if this is fixed, we might be able to drop it.
+class TorLauncher:
+    def __init__(self, reactor, data_directory, log_file=None):
+        if data_directory:
+            data_directory.mkdir(parents=True, exist_ok=True, mode=0o700)
+        self.data_directory = data_directory
+        self._reactor = reactor
+        self.tor_log_file = None
+        self._tor = None
+        self._state = None
+        if log_file:
+            self.tor_log_file = log_file.open("w")
 
-    def write(self, msg):
-        tqdm.write(f"{self.prefix}{msg.strip()}")
+    @property
+    def tor(self):
+        return self._tor
 
+    @property
+    def state(self):
+        return self._state
+
+    def _log(self, msg):
+        if self.tor_log_file:
+            self.tor_log_file.write(msg)
+
+    def close(self):
+        if self.tor_log_file:
+            self.tor_log_file.close()
+
+    async def launch(self):
+        if self._tor is not None:
+            # XXX do I need to do something to clean it up?
+            self._tor = None
+            self._state = None
+
+        tor = await txtorcon.launch(
+            self._reactor,
+            data_directory=self.data_directory,
+            kill_on_stderr=False,
+            progress_updates=lambda x, y, z: print(f"{x}%: {y} - {z}"),
+        )
+
+        tor.protocol.add_event_listener("INFO", self._log)
+        print("🏁 Started Tor version {}".format(tor.version))
+        self._state = await tor.create_state()
+        self._tor = tor
 
 async def main(reactor):
     with open("exitmap-results.csv", "w") as out_file:
@@ -115,29 +159,24 @@ async def main(reactor):
         #    UNIXClientEndpoint(reactor, "/var/run/tor/control")
         # )
 
-        print("🚂 Starting tor")
-        try:
-            tor = await txtorcon.launch(
-                reactor,
-                stdout=TorLogger("  "),
-                stderr=TorLogger("ERR: "),
-                kill_on_stderr=False,
-                progress_updates=lambda x, y, z: print(f"{x}%: {y} - {z}"),
-            )
-        except Exception as exc:
-            print(f"FAILED to start tor {exc}")
-            return
+        data_directory = Path("~/.config/txexitmap/tor_datadir").expanduser()
+        tor_launcher = TorLauncher(reactor, data_directory)
 
-        print("🏁 Started Tor version {}".format(tor.version))
+        print("🚂 Starting tor")
+        await tor_launcher.launch()
 
         exit_list = await get_exit_list(reactor)
-        state = await tor.create_state()
         for relay in tqdm(exit_list):
             try:
-                await measure_relay(reactor, tor, state, relay, URL_LIST, csvwriter)
+                await measure_relay(reactor, tor_launcher, relay, URL_LIST, csvwriter)
+            except TorDisconnectError:
+                tqdm.write("Tor disconnected unexpectectedly, restarting")
+                await tor_launcher.launch()
+                continue
             except Exception as exc:
                 tqdm.write(f"FAILED to measure via {relay['fingerprint']} {exc}")
 
+        tor_launcher.close()
 
 @react
 def _main(reactor):
